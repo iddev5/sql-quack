2783c9869ae95adb7972d349332b26493f44e48c
iddev5 <ayushbardhan5@gmail.com>
2026-05-09 12:56:58 +0530

    Problem set page initial work

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,17 @@
 import Navbar from "@/components/navbar";
+import Link from "next/link";
+import { Play } from "lucide-react";
+
+const problems = [
+  {
+    id: 1,
+    title: 'Basic select query'
+  },
+  {
+    id: 2,
+    title: 'Some aggregate functions'
+  }
+]
 
 export default function Dashboard() {
   return <div className="bg-primary w-screen h-screen overflow-x-hidden">
@@ -9,6 +22,22 @@ export default function Dashboard() {
           <div className="flex items-center gap-4">
             <p className="uppercase text-outline font-inter text-[11px] text-[#948ea1]">Problems</p>
           </div>
+        </div>
+        <div className="p-8 px-16">
+        {
+          problems.map(p =>
+            <div key={p.id} className="text-white bg-secondary p-4 my-4 rounded-lg flex justify-between hover:scale-102 transition-all hover:border-1 border-gray-800">
+              <div>
+                <span className="text-indigo-400 mr-4">{p.id}</span>
+                {p.title}
+              </div>
+              <Link href="/" className="bg-indigo-400 p-1 px-3 rounded-lg hover:bg-indigo-500 flex justify-center items-center gap-1">
+                Solve
+                <Play size={14} fill="white" />
+              </Link>
+            </div>
+          )
+        }
         </div>
       </div>
       <div className="w-[35%] h-screen bg-primary">
